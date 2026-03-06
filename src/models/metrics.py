ae6261f443e2e48cff58093cff2d243a87a50b4f
Saumya Bhardwaj <raaz13bharadwaj@gmail.com>
2026-03-06 21:08:37 +0530

    Patch: Correct NGCD metric formula which gives 0 denominator

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -14,10 +14,10 @@ def recall_at_k(recommended, relevant, k):
 def ndcg_at_k(recommended, relevant, k):
     recommended = recommended[:k]
 
-    dcg = 0
+    dcg = 0.0
     for i, item in enumerate(recommended):
         if item in relevant:
-            dcg += 1 / np.log2(i+1)
+            dcg += 1 / np.log2(i+2)
     
     ideal_hits = min(len(relevant), k)
     idcg = sum(1 / np.log2(i+2) for i in range(ideal_hits))
