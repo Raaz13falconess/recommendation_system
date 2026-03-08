675ce271d5a2180ce7624d3b9e2dd50966ba9b5b
Saumya Bhardwaj <raaz13bharadwaj@gmail.com>
2026-03-09 02:39:17 +0530

    feat: convert model indices to MovieLens movie IDs

--- recommender_service/inference/recommender_engine.py
+++ recommender_service/inference/recommender_engine.py
@@ -1,12 +1,16 @@
+import json
 import numpy as np
 
 
 class RecommenderEngine:
 
-    def __init__(self, user_embedding_path, item_embedding_path):
+    def __init__(self, user_embedding_path, item_embedding_path, movie_map_path):
 
         self.user_embeddings = np.load(user_embedding_path)
         self.item_embeddings = np.load(item_embedding_path)
+
+        with open(movie_map_path) as f:
+            self.movie_map = json.load(f)
 
     def recommend(self, user_id, k=10, seen_items=None):
 
@@ -17,6 +21,8 @@ class RecommenderEngine:
         if seen_items is not None:
             scores[seen_items] = -1e9
 
-        top_k = np.argsort(scores)[-k:][::-1]
+        top_k_idx = np.argsort(scores)[-k:][::-1]
 
-        return top_k
+        movie_ids = [self.movie_map[str(i)] for i in top_k_idx]
+
+        return movie_ids
